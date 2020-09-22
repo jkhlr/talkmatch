c4f62f030284ad6e5f0600864ad7c54fc514b5a4
jkhlr <13289385+jkhlr@users.noreply.github.com>
2020-09-22 16:48:20 +0200

    add root redirect to admin

--- talkmatch/urls.py
+++ talkmatch/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, reverse_lazy
+from django.views.generic import RedirectView
 
 from match.sms_adapter import SmsAdapter
 
 urlpatterns = [
+    path('sms-webhook/', SmsAdapter.as_view()),
+    path('', RedirectView.as_view(url=reverse_lazy('admin:index')))
     path('admin/', admin.site.urls),
-    path('sms-webhook/', SmsAdapter.as_view())
 ]
