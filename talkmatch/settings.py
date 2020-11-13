04c0cce96580fca8707155ff65f7c96714f766d5
jkhlr <13289385+jkhlr@users.noreply.github.com>
2020-11-13 17:15:51 +0100

    rename env variable

--- talkmatch/settings.py
+++ talkmatch/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv(
-    'SECRET_KEY'
+    'DJANGO_SECRET_KEY'
     '70ls@gi^ar)=s%_0*@tlv(au2h_r7e-6il)tcvymg9!btbl1yh'
 )
 
